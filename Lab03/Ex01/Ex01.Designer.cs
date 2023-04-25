42747d994a9ce6df7c584b83d193e044b8b3678b
tuilakhanh <bacdau852@gmail.com>
2023-04-25 15:34:34 +0700

    Inital

---
+++ Lab03/Ex01/Ex01.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Lab03
+{
+	partial class Ex01
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// Ex01
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Name = "Ex01";
+			Text = "Ex01";
+			FormClosed += Ex01_FormClosed;
+			ResumeLayout(false);
+		}
+
+		#endregion
+	}
+}
---
+++ Lab03/Ex01/Ex01.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab03
+{
+	public partial class Ex01 : Form
+	{
+		public Ex01()
+		{
+			InitializeComponent();
+		}
+
+		private void Ex01_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			var MainForm = new MainForm();
+			MainForm.Show();
+		}
+	}
+}
