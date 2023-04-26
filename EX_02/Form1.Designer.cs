f9ca2c08066ee9d78839b6b8672255f7d12f2bcf
nguyenhung1402 <21522118@gm.uit.edu.vn>
2023-04-26 10:45:37 +0700

    Ex02

---
+++ EX_02/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EX_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btbLis = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btbLis
+            // 
+            this.btbLis.Location = new System.Drawing.Point(697, 13);
+            this.btbLis.Name = "btbLis";
+            this.btbLis.Size = new System.Drawing.Size(91, 45);
+            this.btbLis.TabIndex = 1;
+            this.btbLis.Text = "LISTEN";
+            this.btbLis.UseVisualStyleBackColor = true;
+            this.btbLis.Click += new System.EventHandler(this.btbLis_Click);
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(11, 60);
+            this.listView.Margin = new System.Windows.Forms.Padding(2);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(777, 379);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btbLis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btbLis;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ EX_02/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+namespace EX_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btbLis_Click(object sender, EventArgs e)
+        {
+
+            CheckForIllegalCrossThreadCalls = false;
+            Thread severthread = new Thread(new ThreadStart(StartThread));
+            severthread.Start();
+        }
+        private void StartThread()
+        {
+            listView.Items.Add(new ListViewItem("Waiting for connetion"));
+            int bytesRecv = 0;
+            byte[] recv = new byte[1];
+            Socket clientSocket;
+
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipepSV = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepSV);
+            listenerSocket.Listen(-1);
+            clientSocket = listenerSocket.Accept();
+
+            listView.Items.Add(new ListViewItem("New client conneted"));
+
+            while (clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesRecv = clientSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                }
+                while (text[text.Length - 1] != '\n');
+                listView.Items.Add(new ListViewItem("You said: " + text));
+                if (string.IsNullOrEmpty(text.Trim()))  
+                {
+                    Application.Exit();  
+                }
+            }
+
+            listenerSocket.Close();
+        }
+    }
+}
