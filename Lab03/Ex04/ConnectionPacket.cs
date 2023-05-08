db32eabd3a2b665573a8aefdabae93b6b53163a0
tuilakhanh <bacdau852@gmail.com>
2023-05-09 06:22:17 +0700

    Ex04:DirectMessage Form

--- Lab03/Ex04/ConnectionPacket.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab03.Ex04
-{
-	public class UserConnectionPacket
-	{
-		public string Username { get; set; }
-		public bool IsJoining { get; set; }
-		public List<string> Users { get; set; }
-	}
-}
